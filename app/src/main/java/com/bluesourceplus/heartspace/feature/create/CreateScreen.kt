3f618ede2f7c0dddd97cbb778c64a23fb8e59c62
Sergey Hayriyan <sega1ness@gmail.com>
2025-09-13 14:59:44 +0400

    Relayout

--- app/src/main/java/com/bluesourceplus/heartspace/feature/create/CreateScreen.kt
+++ app/src/main/java/com/bluesourceplus/heartspace/feature/create/CreateScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CameraAlt
@@ -32,6 +33,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -137,7 +139,7 @@ fun CreateScreen(
                     Image(
                         painter = rememberAsyncImagePainter(contentState.imageUri),
                         contentDescription = "Selected Mood Image",
-                        modifier = Modifier.size(128.dp),
+                        modifier = Modifier.size(128.dp).clip(RoundedCornerShape(12.dp)),
                         contentScale = ContentScale.Crop
                     )
                 }
